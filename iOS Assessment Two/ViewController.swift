bc37e3e87d2e158a714ab093c45e0fba161031ea
Zachary Harper <zacharyharper@macbook-pro-8.local>
2018-10-25 09:53:37 -0500

    Initial Commit

---
+++ iOS Assessment Two/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Assessment Two
+//
+//  Created by Zachary Harper on 10/25/18.
+//  Copyright © 2018 Zachary Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
