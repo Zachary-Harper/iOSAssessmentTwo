302842d6d38178c5b9db2c4bc95ea0e6dc552e51
Zachary Harper <zacharyharper@macbook-pro-8.local>
2018-10-25 12:01:36 -0500

    ITS DONE, Everything is working

--- iOS Assessment Two/ViewController.swift
+++ iOS Assessment Two/ViewController.swift
@@ -8,12 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        guard
+        let firstName = firstNameTextField.text , !firstName.isEmpty,
+        let lastName = lastNameTextField.text , !lastName.isEmpty,
+        let email = emailTextField.text , !email.isEmpty
+        else {
+            let errorAlert = UIAlertController(title: "Error", message: "Please fill all fields or press cancel to return to login screen.", preferredStyle: UIAlertController.Style.alert)
+            let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) {UIAlertAction in }
+            errorAlert.addAction(dismissAction)
+            self.present(errorAlert, animated: true, completion: nil)
+            return
+            
+            
+            
+        }
+       let personAlert = UIAlertController(title: "Greetings", message: "Hello \(firstName) \(lastName). Your email is \(email)", preferredStyle: UIAlertController.Style.alert)
+        
+        let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) {UIAlertAction in }
+        
+        personAlert.addAction(dismissAction)
+        self.present(personAlert, animated: true, completion: nil)
+        return
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
 
 
 }
